b5a142dc685007aeddee72d21ee074cffab19317
rlfrahm <frahmryan@gmail.com>
2014-09-08 15:05:35 -0500

    build_luggage_isu now uses luggage.sh functions

--- scripts/build_luggage_isu.sh
+++ scripts/build_luggage_isu.sh
@@ -10,76 +10,21 @@
 # git clone git@github.com:isubit/luggage_isu.git
 # cd luggage_isu
 # ./scripts/build_luggage_isu.sh
-# 
+#
 
-ALIAS="@self"
+# Inlcude luggage functions for building
+. $(dirname $0)/libs/luggage.sh
 
-# Ensure we are at the root of a Drupal site.
-DRUPALROOT=$(drush site-alias $ALIAS --component=root)
-DIRECTORY=$(pwd)
-
-if [ "$DIRECTORY" != "$DRUPALROOT" ]; then
-  echo "Please run $0 from the root of a Drupal site." && exit 1
-fi
-
-if [ -z "$DBCREDS" ]; then
-    printf "DB username: "
-    read username
-    stty -echo
-    printf "DB password: "
-    read password
-    stty echo
-    DBCREDS=$username:$password
-else
-    DBCREDS=$DBCREDS
-fi
-
-# Get submodules defined in .gitmodules
-git submodule update --init
-
-# Install luggage, all its features and all its dependencies.
-# Install Drupal 7 using the minimal profile.
-drush $ALIAS si minimal -y --db-url=mysql://$DBCREDS@localhost/luggage_isu --site-name=luggage_isu --account-name=adminn install_configure_form.update_status_module='array(FALSE,FALSE)'
+init
+install_site
 
 # Install Theme - Suitcase
 drush $ALIAS en -y suitcase
 drush $ALIAS vset theme_default suitcase
 
-##Install all the Luggage features.
-drush -v $ALIAS en -y luggage_announcements luggage_biblio luggage_ckeditor luggage_contrib luggage_core luggage_events luggage_events_solr luggage_indicator luggage_news luggage_placeholder luggage_people luggage_people_solr luggage_projects luggage_resources luggage_resources_solr luggage_roles luggage_roles_solr luggage_seo luggage_solr luggage_ui luggage_vars
+install_luggage_features
 
-# Install suitcase features
+# Install luggage_isu features
 drush -v $ALIAS en -y luggage_pubcookie luggage_piwik luggage_people_isu suitcase_config
 
-# Revert all the Luggage features.
-drush $ALIAS fra -y
-
-# Clear all Drupal cache.
-drush $ALIAS cc all
-drush $ALIAS cron-run all --cli all > /dev/null 2>&1
-
-# new files directory
-mkdir -p $DIRECTORY/files
-sudo chown -R $APACHEUSER $DIRECTORY/files
-sudo chmod -R g+w $DIRECTORY/files
-
-# Add a conditional include for settings.env.inc
-echo "
-// Environment specific settings.
-\$environment_settings = '/var/www/env/settings.env.inc';
-
-if (file_exists(\$environment_settings)) {
-require(\$environment_settings);
-}" | sudo tee -a $DIRECTORY/sites/default/settings.php
-
-# Check for existence of /var/www/env/settings.env.inc
-if [ ! -e /var/www/env/settings.env.inc ]; then
-  echo "Warning: /var/www/env/settings.env.inc does not exist"
-fi
-
-# Commit all the additions and switch to development branch.
-echo "Your luggage is ready."
-echo ""
-
-# Provide a one-time login URL
-drush $ALIAS uli
+finish
