2435ee26d2ba83771b210fb38d120e5300a7d05a
rlfrahm <frahmryan@gmail.com>
2014-09-08 12:48:28 -0500

    Removing bash read

--- scripts/build_luggage.sh
+++ scripts/build_luggage.sh
@@ -21,7 +21,6 @@ DIRECTORY=$(pwd)
 # http://stackoverflow.com/questions/1371261/get-current-directory-name-without-full-path-in-bash-script?answertab=active#tab-top
 BASENAME=${PWD##*/}
 echo "Proceeding with site name -> " $BASENAME
-read
 
 if [ "$DIRECTORY" != "$DRUPALROOT" ]; then
   echo "Please run $0 from the root of a Drupal site." && exit 1
